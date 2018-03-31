529d4baac0dc27e370c9207617cb9359636d375d
lallhar <lfc5timesktt@gmail.com>
2018-03-31 14:49:00 +0530

    First commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,44 @@
+
 module.exports = {
-  plugins: [`gatsby-plugin-netlify-cms`],
+  siteMetadata: {
+    title: 'Squarespace',
+  },
+  
+  plugins: [
+
+
+{
+    resolve: `gatsby-plugin-less`,
+    options: {
+      path: `${__dirname}/src/layouts`,
+      name: 'pages',
+      },
+    }, 
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      path: `${__dirname}/src/pages`,
+      name: 'pages',
+    },
+  },
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      path: `${__dirname}/src/images`,
+      name: 'images',
+    },
+  },
+  'gatsby-plugin-react-helmet'
+],
+
+
+
+
 };
+  
+
+
+
 
 
 
